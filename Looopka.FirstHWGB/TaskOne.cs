d422444bc0c5e0297b63027d74269504da24436b
looopka <forployka@gmail.com>
2023-07-07 11:53:12 +0300

    fix start function

--- Looopka.FirstHWGB/TaskOne.cs
+++ Looopka.FirstHWGB/TaskOne.cs
@@ -6,21 +6,13 @@ static int start()
     Console.WriteLine("Привет, выбери доступную функцию:");
     Functions.PrintFunctions();
     string answer = Console.ReadLine();
-    Functions.GetTask(answer);
-    switch (answer)
-    {
-        case "quit":
-        return 1;
-        default: return 0;
-
-    }
+    return Functions.GetTask(answer);
 
 }
 int status = 0;
 while (status == 0)
 {
     status = start();
-    
     
 }
     
